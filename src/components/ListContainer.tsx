5aafe2f92fcce03fdd2f2a711b25a3acbe724629
osvaldo camacho <valdcam87@gmail.com>
2025-02-26 16:24:50 -0600

    a bug was fixed, for the touch sensors in the drag and drop feature

--- src/components/ListContainer.tsx
+++ src/components/ListContainer.tsx
@@ -1,8 +1,7 @@
 import { useTodo } from "../hooks/useTodo"
-import { DndContext, DragEndEvent, closestCenter } from "@dnd-kit/core"
+import { DndContext, DragEndEvent, closestCenter, PointerSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core"
 import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable"
 import TodoItem from "./TodoItem"
-
 
 export default function ListContainer() {
     const {state, dispatch, unCompletedLength} = useTodo()
@@ -13,7 +12,11 @@ export default function ListContainer() {
         if(state.filter === 'completed') return todo.completed
         return true
     })
-    const handleDragEnd = (event:DragEndEvent) => {
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(TouchSensor)
+    )
+    const handleDragEnd = (event: DragEndEvent) => {
         const {active, over} = event
         if(over && active.id !== over.id){
             dispatch({type: 'reorder', payload: {activeId: active.id.toString(), overId: over.id.toString()}})
@@ -25,49 +28,28 @@ export default function ListContainer() {
             <DndContext
                 collisionDetection={closestCenter}
                 onDragEnd={handleDragEnd}
+                sensors={sensors}
             >
                 <SortableContext 
                     items={filteredTodos.map(todo => todo.id)}
                     strategy={verticalListSortingStrategy}
                 >
-            <div className={`w-full bg-white rounded-md text-VeryDarkGrayishBlue md:max-w-[800px] `}>
-                
-                    {filteredTodos.map(todo =>(
-                        <TodoItem
-                            key={todo.id}
-                            todo={todo}
-                        />
-                        // <div 
-                        //     className={`${state.DarkMode ? 'bg-stone-700 text-white border-slate-400': ''} flex justify-between items-center gap-3 p-5 pb-10 border-b last-of-type:border-0`}
-                        //     key={todo.id}
-                        // >
-                        //     <input 
-                        //         type="checkbox"
-                        //         className="check"
-                        //         id="completed"
-                        //         checked={todo.completed}
-                        //         onChange={()=>{dispatch({type: 'toggle-todo', payload: {id: todo.id}})}}
-                                
-                        //     />
-                        //     <p className={`flex-1 ${todo.completed && 'line-through'}`}>{todo.text}</p>
-                        //     <img 
-                        //         className="cursor-pointer" 
-                        //         src="/img/icon-cross.svg" alt="delete" 
-                        //         onClick={()=>{dispatch({type: 'delete-todo', payload: {deleted: todo.id}})}}
-                        //     />
-                        // </div>
-                    ))}
-                
-            </div>
-            </SortableContext>
-            
+                    <div className={`w-full bg-white rounded-md text-VeryDarkGrayishBlue md:max-w-[800px] overflow-container`}>
+                        {filteredTodos.map(todo => (
+                            <TodoItem
+                                key={todo.id}
+                                todo={todo}
+                            />
+                        ))}
+                    </div>
+                </SortableContext>
             </DndContext>    
             <div 
                 className={`mt-5 flex justify-between items-center p-5 rounded-md md:justify-center md:gap-96 ${state.DarkMode ? 'bg-stone-700 text-white': 'bg-white'}`}
-                >
+            >
                 <span>{unCompletedLength} items left</span>
                 <button
-                    onClick={()=>{dispatch({type: 'clear-completed'})}}
+                    onClick={() => {dispatch({type: 'clear-completed'})}}
                 >
                     clear completeds
                 </button>
